37a66edcb001bb03e173b36c17a75efea96a56a5
kostyan <kostya.ignatev.14@mail.ru>
2020-12-14 23:00:30 +0300

    Добавлены необходимые структуры данных, сделаны наброски класса калькулятора

---
+++ calculator/calculator.py
@@ -0,0 +1,8 @@
+class Calculator():
+   
+   def __init__(self, formula_list):
+      self.formula = formula_list
+
+   def check(self):
+      '''Проверка введенной формулы на корректность'''
+      pass
