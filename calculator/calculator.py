04453e188c64d633d52735ae950709e0865133f9
kostyan <kostya.ignatev.14@mail.ru>
2020-12-15 18:13:22 +0300

    Сделано добавление в массив формулы специальных символов

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,8 +1,44 @@
 class Calculator():
    
-   def __init__(self, formula_list):
-      self.formula = formula_list
+   def __init__(self, formula_str):
+      self._operators = ['+', '-', '*', '/', '^', '-', 'cos', 'sin', 'tg', 'ctg', 'ln', 'log', 'sqrt', 'pi', 'e', '(', ')']
+      self._formula = self.split_into_array(formula_str)
+
+   def __str__(self):
+      return str(self._formula)
+
+   def split_into_array(self, formula_str):
+      
+      split_array = []
+
+      for i in range(len(formula_str)):
+         if formula_str[i].isalpha():
+
+            for j in range(2, 5):
+
+               if formula_str[i: i + j] in self._operators:
+                  split_array.append(formula_str[i: i + j])
+                  i = i + j
+
+            if i > len(formula_str) - 1:
+               break
+
+            if formula_str[i] in self._operators:
+               split_array.append(formula_str[i])
+
+         elif formula_str[i] in self._operators:
+            split_array.append(formula_str[i])
+         '''else:
+            if formula_str[i].isdigit():
+               if '''
+      return split_array
 
    def check(self):
       '''Проверка введенной формулы на корректность'''
       pass
+
+#test_str = Calculator('1 + 4 - 5 + 6 + cos(pi / 4)')
+
+test_str = Calculator('cosu67+-*/^-6r6urru7ru7676rsi--nmyu^^^^^muyib    tgctyu,i,yoglnr6u6utrlogr6u6rsqrtpiru+-/67r67*****e-----ur////6u6rur')
+
+print(test_str)
